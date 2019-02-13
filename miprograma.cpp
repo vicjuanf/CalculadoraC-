1dd648c4f8b61f078fd21c8e80f6d3b763d3a4a2
vicjuanf <vicjuanf@gmail.com>
2019-02-13 08:42:51 +0100

    Add files via upload
    Nueva versión de mi programa. Cambios: 
    * Añadido nuevas líneas de código para leer un dato y multiplicarlo por 10
    * Corregido el problema de ....

--- miprograma.cpp
+++ miprograma.cpp
@@ -3,8 +3,18 @@
 using namespace std;
 
 int main(){
+	
+	int dato;
+
 
 	cout << "Hola Mundo\n";
+	cout << "Introduce un dato: ";
+
+	cin >> dato;
+
+	dato = dato * 10;
+
+	cout << "Resultado : " << dato << endl;
 
 
 }
